51a3b75284a3a307d3d414408a0b1d8842750ea5
Alex <alexanderjgp02@gmail.com>
2026-03-10 01:58:52 -0400

    Agregué hora al mensaje de WhatsApp

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'dart:async';
+import 'package:url_launcher/url_launcher.dart';
+
 
 void main() {
   runApp(const FussionExpressApp());
@@ -29,8 +31,42 @@ class CalculadoraScreen extends StatefulWidget {
 }
 
 class _CalculadoraScreenState extends State<CalculadoraScreen> {
+  String horaActualizacion = "";
+  Future<void> enviarWhatsApp() async {
+  String hora = DateFormat('hh:mm a').format(DateTime.now());
+  String monedaEnvia = _tipoOperacion == "COP a Bs" ? "COP" : "Bs";
+  String monedaRecibe = _tipoOperacion == "COP a Bs" ? "Bs" : "COP";
 
-  final TextEditingController _montoController = TextEditingController();
+  String envia = _formatter.format(
+      int.parse(_enviaController.text.replaceAll('.', '')));
+
+  String recibe = _formatter.format(
+      int.parse(_recibeController.text.replaceAll('.', '')));
+
+  String tasaUsada = _tipoOperacion == "COP a Bs"
+      ? tasaCopABs.toString()
+      : tasaBsACop.toString();
+
+  String mensaje = """
+Hola, quiero realizar este cambio:
+
+Envio: $envia $monedaEnvia
+Recibo: $recibe $monedaRecibe
+Tasa ${monedaEnvia}→${monedaRecibe}: $tasaUsada
+Hora: $hora
+""";
+
+  final url = Uri.parse(
+    "https://wa.me/584126145680?text=${Uri.encodeComponent(mensaje)}"
+  );
+
+  if (await canLaunchUrl(url)) {
+    await launchUrl(url);
+  }
+}
+
+  final TextEditingController _enviaController = TextEditingController();
+  final TextEditingController _recibeController = TextEditingController();
 
   double tasaCopABs = 6.6;
   double tasaBsACop = 5.5;
@@ -38,10 +74,11 @@ class _CalculadoraScreenState extends State<CalculadoraScreen> {
   DateTime ultimaActualizacion = DateTime.now();
   int segundosDesdeActualizacion = 0;
 
-  String _resultado = "";
   String _tipoOperacion = "COP a Bs";
 
   final NumberFormat _formatter = NumberFormat("#,###", "es_ES");
+
+  bool _actualizando = false;
 
   @override
   void initState() {
@@ -57,27 +94,23 @@ class _CalculadoraScreenState extends State<CalculadoraScreen> {
       setState(() {
         segundosDesdeActualizacion =
             DateTime.now().difference(ultimaActualizacion).inSeconds;
+            horaActualizacion = DateFormat('hh:mm a').format(DateTime.now());
       });
     });
   }
 
   Future<void> cargarTasas() async {
-
     try {
-
       final url = Uri.parse(
-        "https://docs.google.com/spreadsheets/d/1WksYIbs2K9x9JPvuT1qypJnhZr3Pgo1B2BfjC4GvgEc/gviz/tq?tqx=out:json"
-      );
+          "https://docs.google.com/spreadsheets/d/1WksYIbs2K9x9JPvuT1qypJnhZr3Pgo1B2BfjC4GvgEc/gviz/tq?tqx=out:json");
 
       final response = await http.get(url);
 
       String data = response.body;
-
       data = data.substring(47);
       data = data.substring(0, data.length - 2);
 
       final jsonData = json.decode(data);
-
       final rows = jsonData["table"]["rows"];
 
       setState(() {
@@ -87,41 +120,80 @@ class _CalculadoraScreenState extends State<CalculadoraScreen> {
         ultimaActualizacion = DateTime.now();
         segundosDesdeActualizacion = 0;
       });
-
     } catch (e) {
       print("Error cargando tasas: $e");
     }
   }
 
-  void _limpiar() {
-    setState(() {
-      _montoController.clear();
-      _resultado = "";
-    });
+ void calcularDesdeEnvia(String value) {
+
+  if (_actualizando) return;
+
+  _actualizando = true;
+
+  String clean = value.replaceAll('.', '');
+  double monto = double.tryParse(clean) ?? 0;
+
+  double resultado;
+
+  if (_tipoOperacion == "COP a Bs") {
+    resultado = monto / tasaCopABs;
+  } else {
+    resultado = monto * tasaBsACop;
   }
 
-  void _calcular() {
+  String formatted = _formatter.format(resultado.round());
+
+  _recibeController.value = TextEditingValue(
+    text: formatted,
+    selection: TextSelection.collapsed(offset: formatted.length),
+  );
+
+  _actualizando = false;
+}
+
+  void calcularDesdeRecibe(String value) {
+
+  if (_actualizando) return;
+
+  _actualizando = true;
+
+  String clean = value.replaceAll('.', '');
+  double monto = double.tryParse(clean) ?? 0;
+
+  double resultado;
+
+  if (_tipoOperacion == "COP a Bs") {
+    resultado = monto * tasaCopABs;
+  } else {
+    resultado = monto / tasaBsACop;
+  }
+
+  String formatted = _formatter.format(resultado.round());
+
+  _enviaController.value = TextEditingValue(
+    text: formatted,
+    selection: TextSelection.collapsed(offset: formatted.length),
+  );
+
+  _actualizando = false;
+}
+
+  void limpiar() {
     setState(() {
-
-      String montoRaw = _montoController.text.replaceAll('.', '');
-      double monto = double.tryParse(montoRaw) ?? 0;
-
-      double total;
-
-      if (_tipoOperacion == "COP a Bs") {
-        total = monto / tasaCopABs;
-      } else {
-        total = monto * tasaBsACop;
-      }
-
-      _resultado =
-          "${_formatter.format(total.round())} ${_tipoOperacion == "COP a Bs" ? 'Bs' : 'COP'}";
-
+      _enviaController.clear();
+      _recibeController.clear();
     });
+    
+    
   }
 
   @override
   Widget build(BuildContext context) {
+
+    String monedaEnvia = _tipoOperacion == "COP a Bs" ? "COP" : "Bs";
+    String monedaRecibe = _tipoOperacion == "COP a Bs" ? "Bs" : "COP";
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Fusión Express"),
@@ -146,25 +218,23 @@ class _CalculadoraScreenState extends State<CalculadoraScreen> {
 
                     const Text(
                       "🟢 Tasa en vivo",
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                     ),
 
                     Text(
                       "Actualizado hace $segundosDesdeActualizacion s",
                       style: const TextStyle(color: Colors.white70),
                     ),
+                     Text(
+                       "Hora: $horaActualizacion",
+                         style: const TextStyle(color: Colors.white70),
+                     ),  
 
                     const SizedBox(height: 15),
 
-                    Text(
-                      "COP ➜ Bs: $tasaCopABs",
-                      style: const TextStyle(fontSize: 16),
-                    ),
-
-                    Text(
-                      "Bs ➜ COP: $tasaBsACop",
-                      style: const TextStyle(fontSize: 16),
-                    ),
+                    Text("COP ➜ Bs: $tasaCopABs"),
+                    Text("Bs ➜ COP: $tasaBsACop"),
 
                     const SizedBox(height: 20),
 
@@ -181,6 +251,7 @@ class _CalculadoraScreenState extends State<CalculadoraScreen> {
                           onPressed: () {
                             setState(() {
                               _tipoOperacion = "COP a Bs";
+                              limpiar();
                             });
                           },
                           child: const Text("COP a Bs"),
@@ -197,6 +268,7 @@ class _CalculadoraScreenState extends State<CalculadoraScreen> {
                           onPressed: () {
                             setState(() {
                               _tipoOperacion = "Bs a COP";
+                              limpiar();
                             });
                           },
                           child: const Text("Bs a COP"),
@@ -204,66 +276,93 @@ class _CalculadoraScreenState extends State<CalculadoraScreen> {
                       ],
                     ),
 
+                    const SizedBox(height: 20),
+
+                    TextField(
+  controller: _enviaController,
+  keyboardType: TextInputType.number,
+  decoration: InputDecoration(
+    labelText: "Envía ($monedaEnvia)",
+  ),
+  onChanged: (value) {
+
+    if (value.isEmpty) {
+      calcularDesdeEnvia(value);
+      return;
+    }
+
+    String clean = value.replaceAll('.', '');
+
+    if (clean.isNotEmpty) {
+
+      String formatted = _formatter.format(int.parse(clean));
+
+      if (formatted != value) {
+
+        _enviaController.value = TextEditingValue(
+          text: formatted,
+          selection: TextSelection.collapsed(offset: formatted.length),
+        );
+      }
+    }
+
+    calcularDesdeEnvia(_enviaController.text);
+  },
+),
+
                     const SizedBox(height: 15),
 
                     TextField(
-                      controller: _montoController,
-                      keyboardType: TextInputType.number,
-                      style: const TextStyle(color: Colors.white),
-                      decoration: const InputDecoration(
-                        labelText: 'Monto',
-                        labelStyle: TextStyle(color: Colors.white70),
-                      ),
-                      onChanged: (value) {
+  controller: _recibeController,
+  keyboardType: TextInputType.number,
+  decoration: InputDecoration(
+    labelText: "Recibe ($monedaRecibe)",
+  ),
+  onChanged: (value) {
 
-                        if (value.isEmpty) return;
+    if (value.isEmpty) {
+      calcularDesdeRecibe(value);
+      return;
+    }
 
-                        String clean = value.replaceAll('.', '');
+    String clean = value.replaceAll('.', '');
 
-                        if (clean.isNotEmpty) {
+    if (clean.isNotEmpty) {
 
-                          String formatted =
-                              _formatter.format(int.parse(clean));
+      String formatted = _formatter.format(int.parse(clean));
 
-                          if (value != formatted) {
+      if (formatted != value) {
 
-                            _montoController.value = TextEditingValue(
-                              text: formatted,
-                              selection: TextSelection.collapsed(
-                                  offset: formatted.length),
-                            );
-                          }
-                        }
-                        _calcular();
-                      },
-                    ),
+        _recibeController.value = TextEditingValue(
+          text: formatted,
+          selection: TextSelection.collapsed(offset: formatted.length),
+        );
+      }
+    }
 
-                    const SizedBox(height: 20),
-
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-
-                        ElevatedButton(
-                          onPressed: _limpiar,
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.red,
-                          ),
-                          child: const Text("Limpiar"),
-                        ),
-                      ],
-                    ),
+    calcularDesdeRecibe(_recibeController.text);
+  },
+),
 
                     const SizedBox(height: 20),
 
-                    Text(
-                      _resultado,
-                      style: const TextStyle(
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
+                    ElevatedButton(
+                      onPressed: limpiar,
+                      style:
+                          ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                      child: const Text("Limpiar"),
+                     ),
+                     
+                      const SizedBox(height: 10),
 
+ElevatedButton.icon(
+  onPressed: enviarWhatsApp,
+  icon: const Icon(Icons.chat),
+  label: const Text("Enviar por WhatsApp"),
+  style: ElevatedButton.styleFrom(
+    backgroundColor: Colors.green,
+  ),
+),
                   ],
                 ),
               ),
